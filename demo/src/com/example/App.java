9cf0dbf4105ac33bde0d6e038ffe4ce8b426b037
Javier <35864946+jmagit@users.noreply.github.com>
2026-03-23 13:37:42 +0100

    ejemplo basico

---
+++ demo/src/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.example.ejemplos.Calculadora;
+
+public class App {
+
+	public static void main(String[] args) {
+//		var c = new Calculadora();		
+//		System.out.println(c.suma(2.0, 1.0));
+	}
+
+}
---
+++ demo/src/com/example/ejemplos/Calculadora.java
@@ -0,0 +1,10 @@
+package com.example.ejemplos;
+
+public class Calculadora {
+	public double suma(double a, double b) {
+		return a + b;
+	}
+	public int suma(int a, int b) {
+		return a + a;
+	}
+}
